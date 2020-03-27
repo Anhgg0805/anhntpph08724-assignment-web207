99d00613b63945156221a8d3371f70bf7d883b7b
Anhgg0805 <58631655+Anhgg0805@users.noreply.github.com>
2020-03-27 15:26:14 +0700

    commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import {ProductListComponent} from './product-list/product-list.component';
+import {AboutComponent} from './about/about.component';
+import {ServiceComponent} from './service/service.component';
+import {ProductDetailComponent} from './product-detail/product-detail.component';
+import {ProductManagerComponent} from './product-manager/product-manager.component';
+import {ProductEditComponent} from './product-edit/product-edit.component';
+import {NotFoundComponent} from './not-found/not-found.component';
+
+
+const routes: Routes = [
+  { path: "", redirectTo: "home", pathMatch: "full" },
+  { path: "home", component: ProductListComponent },
+  { path: "about", component: AboutComponent },
+  { path: "service", component: ServiceComponent },
+  { path: "product-manager", component: ProductManagerComponent },
+  {path : 'product-list',component:ProductListComponent},
+  { path: 'product/:id', component: ProductDetailComponent},
+  { path: 'product/edit/:id', component: ProductEditComponent},
+  { path: "404", component: NotFoundComponent },
+  { path: "**", redirectTo: "/404" }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,34 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
-
+import { SlidesComponent } from './slides/slides.component';
+import { ServiceComponent } from './service/service.component';
+import { AboutComponent } from './about/about.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ProductManagerComponent } from './product-manager/product-manager.component';
+import { ProductEditComponent } from './product-edit/product-edit.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [
+    AppComponent,
+    SlidesComponent,
+    ServiceComponent,
+    AboutComponent,
+    ProductListComponent,
+    ProductDetailComponent,
+    ProductManagerComponent,
+    ProductEditComponent,
+    NotFoundComponent,
+    
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
