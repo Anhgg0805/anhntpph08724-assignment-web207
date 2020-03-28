421a63956bc53d86184a22f2ad89495c27450424
Anhgg0805 <anhsowon@gmail.com>
2020-03-29 01:13:06 +0700

    3 files modified

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import {Product} from '../Product';
+import { ProductService } from '../product.service';
+import { ActivatedRoute} from '@angular/router';
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.css']
 })
 export class ProductDetailComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  // @Input('data') product:Product;
+  constructor(
+    private productService: ProductService,
+    private route: ActivatedRoute
+  ) { }
+  product:Product;
+  ngOnInit() {
+    
+    this.getProduct();
+  }
+  getProduct(){
+    this.route.params.subscribe(param => {
+      // this.product = this.productService.getProduct(param.id);
+      this.productService.getProduct(param.id).subscribe(data =>{
+        // console.log(data);
+        this.product=data;
+      })
+      console.log(param);
+    })
   }
 
 }
