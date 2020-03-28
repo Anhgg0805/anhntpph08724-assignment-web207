70044904b0fd97639eaa6b8cabfd3351bfe0d837
Anhgg0805 <anhsowon@gmail.com>
2020-03-29 00:47:25 +0700

    1 file modified

--- src/app/product-manager/product-manager.component.ts
+++ src/app/product-manager/product-manager.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Product} from '../Product';
+import {ProductService} from '../product.service';
+import { ActivatedRoute, Router} from '@angular/router';
 @Component({
   selector: 'app-product-manager',
   templateUrl: './product-manager.component.html',
   styleUrls: ['./product-manager.component.css']
 })
 export class ProductManagerComponent implements OnInit {
+  products:Product[];
+  productNew:Product= new Product();
+  product:Product;
+  constructor(
+    private productService:ProductService,
+    private router: Router
+  ) { }
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    this.getProducts();
   }
-
+  getProducts(){
+    // this.products=this.productService.getProducts();
+    this.productService.getProducts().subscribe(data => {
+      // console.log(data);
+      this.products=data;
+    })
+  }
+  removeItem(id) {
+    // this.products=this.productService.removeProduct(id);
+    this.productService.removeProduct(id).subscribe(response =>{
+      this.router.navigate(['/product-manager'])
+      // console.log(response);
+    });
+  }
+  addProduct(){
+    // this.productService.addProduct(this.productNew);
+    this.productService.addProduct(this.productNew).subscribe(data => {
+    this.router.navigate(['/product-manager'])
+    });
+    
+  }
 }
