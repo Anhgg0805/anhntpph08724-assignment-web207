0c3ec2a55116cff490b9a5caa2caf79ee7e04619
Anhgg0805 <anhsowon@gmail.com>
2020-03-29 01:04:54 +0700

    3 files modified

--- src/app/product-edit/product-edit.component.ts
+++ src/app/product-edit/product-edit.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../product.service';
+import { ActivatedRoute, Router} from '@angular/router';
+import {Product} from '../Product';
 
 @Component({
   selector: 'app-product-edit',
@@ -6,10 +9,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-edit.component.css']
 })
 export class ProductEditComponent implements OnInit {
+  product:Product;
+  constructor(
+    private productService: ProductService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    this.getProduct();
   }
-
+  getProduct(){
+     this.route.params.subscribe(param => {
+      this.productService.getProduct(param.id).subscribe(data =>{
+        // console.log(data);
+        this.product=data;
+      })
+      console.log(param);
+    })
+  }
+  edit(){
+    this.productService.editProduct(this.product).subscribe(data =>{
+      console.log(data);
+      this.router.navigate(['/product-manager']);
+    }
+    );
+    
+  }
 }
